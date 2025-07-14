269091001df3e39b7724d91fced3eee77c172526
Japneet Sachdeva <japneetsachdeva1@gmail.com>
2025-07-14 18:08:55 +0530

    updated test

--- src/test/java/pom/Test1.java
+++ src/test/java/pom/Test1.java
@@ -19,9 +19,13 @@ public class Test1 extends BaseTest
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         loginPage = new LoginPage(driver);
         loginPage.loadApplicationURL();
+        System.out.println(driver.getCurrentUrl());
         homePage = loginPage.loginUser();
+        System.out.println(driver.getCurrentUrl());
         storePage = homePage.clickItem();
+        System.out.println(driver.getCurrentUrl());
         storePage.selectItem();
+        System.out.println(driver.getCurrentUrl());
         Thread.sleep(2000);
     }
 }
