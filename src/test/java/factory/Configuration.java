04fe7e925194385fb0a4679cb5b2451cb3d46938
Japneet Sachdeva <japneetsachdeva1@gmail.com>
2025-07-14 18:00:54 +0530

    more changes

--- src/test/java/factory/Configuration.java
+++ src/test/java/factory/Configuration.java
@@ -3,5 +3,5 @@ package factory;
 public class Configuration
 {
     public static String browserName = System.getProperty("browser", "CHROME");
-    public static String executionMode = System.getProperty("mode", "LOCAL");
+    public static String executionMode = System.getProperty("mode", "GIT");
 }
